e61271f44056e99c05a220976f92d71485719cf4
caleron <caleron@users.noreply.github.com>
2017-11-23 23:10:19 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app light>
+    <v-toolbar fixed app :clipped-left="clipped">
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <v-layout column align-center>
+            <img src="/static/img/v.png" alt="Vuetify.js" class="mb-5">
+            <blockquote>
+              &#8220;First, solve the problem. Then, write the code.&#8221;
+              <footer>
+                <small>
+                  <em>&mdash;John Johnson</em>
+                </small>
+              </footer>
+            </blockquote>
+            <router-view></router-view>
+          </v-layout>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        fixed: false,
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
